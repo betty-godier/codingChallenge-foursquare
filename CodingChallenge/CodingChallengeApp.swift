eec6b8805185f4eae7bda9abf11da3e7be1c8e9c
Betty Godier <betty.godier@gmail.com>
2022-07-10 23:22:25 +0200

    Initial Commit

---
+++ CodingChallenge/CodingChallengeApp.swift
@@ -0,0 +1,14 @@
+//
+//      2022  Betty.dev 
+//
+
+import SwiftUI
+
+@main
+struct CodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodingChallenge/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//      2022  Betty.dev 
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
