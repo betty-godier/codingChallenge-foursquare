d4276ef66f338224d9a76097ad864058f8c31395
Betty Godier <betty.godier@gmail.com>
2022-07-11 23:42:55 +0200

    Add feed API

---
+++ CodingChallenge/Feed API/Helpers + Errors.swift
@@ -0,0 +1,13 @@
+//
+//      2022  Betty Godier
+//      Coding challenge
+//
+
+import Foundation
+
+public class RemoteFeedLoader {
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+}
