f98f56392aa4f5f246851e17737b56d3773fae7e
Betty Godier <betty.godier@gmail.com>
2022-07-15 22:02:55 +0200

    Guarantee we do not deliver or invoke the completion closure after the `RemoteFeedLoader` instance has been allocated

--- CodingChallengeTests/RemoteFeedLoaderTests.swift
+++ CodingChallengeTests/RemoteFeedLoaderTests.swift
@@ -102,6 +102,20 @@ class RemoteFeedLoaderTests: XCTestCase {
         })
     }
     
+    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
+        let url = URLRequest(url: URL(string: "https://a-url.com")!)
+        let client = HTTPClientSpy()
+        var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
+        
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut?.load { capturedResults.append($0) }
+        
+        sut = nil
+        client.complete(withStatusCode: 200, data: makeItemsJSON([]))
+        
+        XCTAssertTrue(capturedResults.isEmpty)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URLRequest = URLRequest(url: URL(string: "https://a-url.com")!), file: StaticString = #file , line: UInt = #line) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
