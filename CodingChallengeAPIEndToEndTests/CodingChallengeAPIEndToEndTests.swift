9d065b7ff81ab3afb5cb66870b1d5a15ba03494d
Betty Godier <betty.godier@gmail.com>
2022-07-17 16:44:15 +0200

    Add new targets for APIEndToEndTests & CI

---
+++ CodingChallengeAPIEndToEndTests/CodingChallengeAPIEndToEndTests.swift
@@ -0,0 +1,17 @@
+//
+//      2022  Betty Godier
+//      Coding challenge
+//
+
+import XCTest
+@testable import CodingChallenge
+
+class CodingChallengeAPIEndToEndTests: XCTestCase {
+
+    func test_endToEndTestsServerGETURLRequest_fromURL() {
+
+    }
+    
+    // Mark: - Helpers
+
+}
